20b4b0ae42fe91ea1fdbcb46ee8af5a874741b2d
prasanganath <sandun.wed@gmail.com>
2019-01-03 22:09:58 +0530

    update

--- MapDirectionSample/app/src/main/java/com/example/user/mapdirectionsample/DataParser.java
+++ MapDirectionSample/app/src/main/java/com/example/user/mapdirectionsample/DataParser.java
@@ -1,7 +1,11 @@
 package com.example.user.mapdirectionsample;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DataParser{
     private HashMap<String, String> getPlace(JSONObject googlePlaceJson){
@@ -41,6 +45,41 @@ public class DataParser{
         }
 
         return googlePlaceMap;
+    }
+
+    private List<HashMap<String,String>> getplaces(JSONArray jsonArray)
+    {
+        int count = jsonArray.length();
+        List<HashMap<String,String>> placesList = new ArrayList<>();
+        HashMap<String,String> placeMap = null;
+
+        for (int i = 0;i<count;i++)
+        {
+            try {
+                placeMap = getPlace((JSONObject) jsonArray.get(i));
+                placesList.add(placeMap);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return placesList;
+
+
+    }
+    public List<HashMap<String,String>> parse(String jsonData)
+    {
+        JSONArray jsonArray = null;
+        JSONObject jsonObject;
+        try {
+
+                jsonObject = new JSONObject(jsonData);
+                jsonArray = jsonObject.getJSONArray("result");
+        } catch (JSONException e) {
+                e.printStackTrace();
+        }
+        return getplaces(jsonArray);
+
     }
 
 }
--- MapDirectionSample/app/src/main/java/com/example/user/mapdirectionsample/GetNearbyPlacesData.java
+++ MapDirectionSample/app/src/main/java/com/example/user/mapdirectionsample/GetNearbyPlacesData.java
@@ -1,8 +1,13 @@
 package com.example.user.mapdirectionsample;
 import android.os.AsyncTask;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 public class GetNearbyPlacesData extends AsyncTask<Object, String, String>{
     String googlePlacesData;
@@ -30,5 +35,27 @@ public class GetNearbyPlacesData extends AsyncTask<Object, String, String>{
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+    }
+    private void showNearbyPlaces(List<HashMap<String,String>> nearbyPlaceList)
+    {
+        for (int i = 0;i<nearbyPlaceList.size() ; i++)
+        {
+            MarkerOptions markerOptions = new MarkerOptions();
+            HashMap<String , String> googlePlace = nearbyPlaceList.get(i);
+
+            String placeName = googlePlace.get("place_name");
+            String vicinity = googlePlace.get("vicinity");
+            double lat = Double.parseDouble( googlePlace.get("lat"));
+            double lng = Double.parseDouble( googlePlace.get("lng"));
+
+            LatLng latLng = new LatLng(lat, lng);
+            markerOptions.position(latLng);
+            markerOptions.title(placeName +" : "+vicinity);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+            mMap.addMarker(markerOptions);
+
+        }
+
     }
 }
